b4c1138967221eb6ba4ba163e06903cf949ef7e7
TheVictorDevX <thevictordevx@gmail.com>
2025-04-10 14:24:33 +0700

    Delete TodoListApp directory

--- TodoListApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.todolistapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.todolistapp to javafx.fxml;
-    exports com.example.todolistapp;
-}
